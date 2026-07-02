cb9061734e9e59939bd337351d0cd1d9fb4f9a0d
Striker <itzstriker561@gmail.com>
2026-07-02 17:28:32 +0100

    @update/ brand app as handoff, add password defaults, OTP two-factor page

--- resources/views/pages/auth/two-factor-challenge.blade.php
+++ resources/views/pages/auth/two-factor-challenge.blade.php
@@ -1,38 +1,61 @@
 <x-layouts::auth :title="__('Two-factor authentication')">
     <div class="flex flex-col gap-6">
-        <x-auth-header :title="__('Two-factor authentication')" :description="__('Please confirm access to your account by entering the authentication code provided by your authenticator application.')" />
-
-        <form method="POST" action="{{ route('two-factor.login') }}" class="flex flex-col gap-6">
-            @csrf
-
-            <flux:input name="code" :label="__('Code')" type="text" inputmode="numeric" required autofocus
-                autocomplete="one-time-code" placeholder="123456" />
-
-            <flux:button variant="primary" type="submit" class="w-full">
-                {{ __('Login') }}
-            </flux:button>
-        </form>
-
-        <div class="relative">
-            <div class="absolute inset-0 flex items-center">
-                <div class="w-full border-t border-zinc-200 dark:border-zinc-700"></div>
+        <div class="relative w-full h-auto" x-cloak x-data="{
+            showRecoveryInput: @js($errors->has('recovery_code')),
+            code: '',
+            recovery_code: '',
+            focusOtp() { this.$nextTick(() => this.$refs.otp?.querySelector('input')?.focus()); },
+            init() { if (!this.showRecoveryInput) this.focusOtp(); },
+            toggleInput() {
+                this.showRecoveryInput = !this.showRecoveryInput;
+                this.code = '';
+                this.recovery_code = '';
+                $nextTick(() => { this.showRecoveryInput ? this.$refs.recovery_code?.focus() : this.focusOtp(); });
+            },
+        }">
+            <div x-show="!showRecoveryInput">
+                <x-auth-header :title="__('Authentication code')" :description="__('Enter the authentication code provided by your authenticator application.')" />
             </div>
-            <div class="relative flex justify-center text-xs uppercase">
-                <span class="px-2 text-zinc-500 dark:text-zinc-400 bg-white dark:bg-zinc-900">
-                    {{ __('Or') }}
-                </span>
+
+            <div x-show="showRecoveryInput">
+                <x-auth-header :title="__('Recovery code')" :description="__('Please confirm access to your account by entering one of your emergency recovery codes.')" />
             </div>
+
+            <form method="POST" action="{{ route('two-factor.login.store') }}">
+                @csrf
+
+                <div class="space-y-5 text-center">
+                    <div x-show="!showRecoveryInput">
+                        <div class="flex items-center justify-center my-5" x-ref="otp">
+                            <flux:otp x-model="code" length="6" name="code" label="OTP Code" label:sr-only
+                                class="mx-auto" />
+                        </div>
+                    </div>
+
+                    <div x-show="showRecoveryInput">
+                        <div class="my-5">
+                            <flux:input type="text" name="recovery_code" x-ref="recovery_code"
+                                x-bind:required="showRecoveryInput" autocomplete="one-time-code"
+                                x-model="recovery_code" />
+                        </div>
+                        @error('recovery_code')
+                            <flux:text color="red">{{ $message }}</flux:text>
+                        @enderror
+                    </div>
+
+                    <flux:button variant="primary" type="submit" class="w-full">{{ __('Continue') }}</flux:button>
+                </div>
+
+                <div class="mt-5 space-x-0.5 text-sm leading-5 text-center">
+                    <span class="opacity-50">{{ __('or you can') }}</span>
+                    <div class="inline font-medium underline cursor-pointer opacity-80">
+                        <span x-show="!showRecoveryInput"
+                            @click="toggleInput()">{{ __('login using a recovery code') }}</span>
+                        <span x-show="showRecoveryInput"
+                            @click="toggleInput()">{{ __('login using an authentication code') }}</span>
+                    </div>
+                </div>
+            </form>
         </div>
-
-        <form method="POST" action="{{ route('two-factor.login') }}" class="flex flex-col gap-6">
-            @csrf
-
-            <flux:input name="recovery_code" :label="__('Recovery code')" type="text"
-                placeholder="XXXX-XXXX-XXXX-XXXX" />
-
-            <flux:button variant="primary" type="submit" class="w-full">
-                {{ __('Use recovery code') }}
-            </flux:button>
-        </form>
     </div>
 </x-layouts::auth>
